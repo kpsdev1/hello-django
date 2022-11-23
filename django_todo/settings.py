5dfb447ada315288354ab790bc702ef6ece2a836
kpsdev1 <stuart151@hotmail.com>
2022-11-23 19:07:36 +0000

    auto deploy

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 DEBUG = True
 
 
-ALLOWED_HOSTS = os.environ.get('HEROKU_HOSTNAME')
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition

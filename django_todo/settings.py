820d261277d671b19e4076eb6ac8bbe0731f75b1
kpsdev1 <stuart151@hotmail.com>
2022-11-23 18:54:03 +0000

    auto deploy

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# test
 SECRET_KEY = os.environ.get('SECRET_KEY', ' ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
